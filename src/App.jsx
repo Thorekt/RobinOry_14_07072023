8369a2b58fd9af4a8f25d849f50bc74b959bfb42
Robin Ory <35566455+Thorekt@users.noreply.github.com>
2023-07-21 19:25:10 +0200

    navbar base style

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CreateEmployee from './app/pages/CreateEmployee';
 import EmployeeList from './app/pages/EmployeeList';
 import Error from './app/pages/Error';
-import Header from './app/components/Header';
+import NavBar from './app/components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './app/styles/app.css';
 
 function App() {
   return (
-    <div className="main">
+    <main>
       <Router>
-        <Header />
+        <NavBar />
         <div className="main-container">
           <Routes>
             <Route index element={<CreateEmployee />} />
@@ -19,7 +20,7 @@ function App() {
           </Routes>
         </div>
       </Router>
-    </div>
+    </main>
   );
 }
 
--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import '../styles/components/navBar.css';
 
 export default function NavBar() {
   return (
-    <nav className="main-nav">
-      <ul>
-        <li>
-          <Link className="main-nav-item" to="/">Home</Link>
-        </li>
-        <li>
-          <Link className="main-nav-item" to="/EmployeeList">Employee List</Link>
-        </li>
-      </ul>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">HRnet</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <ul className="nav nav-pills ">
+          <li className="nav-item">
+            <NavLink activeClassName="active" className="nav-link" to="/">Home</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/EmployeeList">Employee List</NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }

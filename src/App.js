83cd899400642486ffcabf4a6fa4c8d8d51040d1
Robin Ory <35566455+Thorekt@users.noreply.github.com>
2023-07-21 15:35:46 +0200

    creation du header des pages

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CreateEmployee from './app/pages/CreateEmployee';
 import EmployeeList from './app/pages/EmployeeList';
 import Error from './app/pages/Error';
-import NavBar from './app/components/NavBar';
+import Header from './app/components/Header';
+import './app/styles/app.css'
 
 function App() {
   return (
+    <div className='main-container'>
     <Router>
-      <NavBar/>
+      <Header/>
       <Routes>
         <Route index element={<CreateEmployee />} />
         <Route path='/EmployeeList' element={<EmployeeList />} />
         <Route path='/*' element={<Error />} />
       </Routes>
     </Router>
+    </div>
   );
 }
 
---
+++ src/app/components/Header.js
@@ -0,0 +1,11 @@
+import NavBar from "./NavBar";
+import '../styles/components/header.css';
+
+export default function Header(){
+    return(
+        <header className="main-header">
+            <h1>HRnet</h1>
+            <NavBar/>
+        </header>
+    );
+}

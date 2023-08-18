d90b16205c2deb63ea7666497456d82c49bb1ec8
Robin Ory <35566455+Thorekt@users.noreply.github.com>
2023-08-18 16:05:58 +0200

    remplacement du fonctionnement de la table pour utilisé react-table et  que le tableau sois filtrable

--- src/app/components/EmployeeTable.jsx
+++ src/app/components/EmployeeTable.jsx
@@ -1,8 +1,64 @@
 import React from 'react';
 import Table from 'react-bootstrap/Table';
 import { useSelector } from 'react-redux';
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
 import selectEmployee from '../utils/selectors';
-import EmployeeTableRow from './EmployeeTableRow';
+
+const columnHelper = createColumnHelper();
+
+const columns = [
+  columnHelper.accessor('firstName', {
+    header: 'First Name',
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('lastName', {
+    header: 'Last Name',
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('startDate', {
+    header: 'Start Date',
+    cell: (info) => info.renderValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('department', {
+    header: 'Department',
+    cell: (info) => info.renderValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('birthDate', {
+    header: 'Date of Birth',
+    cell: (info) => info.renderValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('street', {
+    header: 'Street',
+    cell: (info) => info.renderValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('city', {
+    header: 'City',
+    cell: (info) => info.renderValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('state', {
+    header: 'State',
+    cell: (info) => info.renderValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('zipCode', {
+    header: 'Zip Code',
+    cell: (info) => info.renderValue(),
+    footer: (info) => info.column.id,
+  }),
+];
 
 /**
  * EmployeeTable component (table)
@@ -13,27 +69,61 @@ export default function EmployeeTable() {
   const { list } = useSelector(selectEmployee);
   const employees = list;
 
+  const [sorting, setSorting] = React.useState([]);
+
+  const table = useReactTable({
+    data: employees,
+    columns,
+    state: {
+      sorting,
+    },
+    onSortingChange: setSorting,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+  });
+
   return (
     <Table>
       <thead>
-        <tr>
-          <th scope="col">First Name</th>
-          <th scope="col">Last Name</th>
-          <th scope="col">Start Date</th>
-          <th scope="col">Department</th>
-          <th scope="col">Date of Birth</th>
-          <th scope="col">Street</th>
-          <th scope="col">City</th>
-          <th scope="col">State</th>
-          <th scope="col">Zip Code</th>
-        </tr>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th key={header.id}>
+                {header.isPlaceholder ? null : (
+                  // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+                  <div
+                    {...{
+                      className: header.column.getCanSort()
+                        ? 'cursor-pointer select-none'
+                        : '',
+                      onClick: header.column.getToggleSortingHandler(),
+                    }}
+                  >
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext(),
+                    )}
+                    {{
+                      asc: ' 🔼',
+                      desc: ' 🔽',
+                    }[header.column.getIsSorted()] ?? null}
+                  </div>
+                )}
+              </th>
+            ))}
+          </tr>
+        ))}
       </thead>
       <tbody>
-        {
-      employees.map((employee) => (
-        <EmployeeTableRow key={employee.id} employee={employee} />
-      ))
-      }
+        {table.getRowModel().rows.map((row) => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <td key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
       </tbody>
     </Table>
   );
